5f52a03c4ab65c74718855a3ddce98567db2bf86
Pavel <grigorov_ps@mail.ru>
2023-10-05 12:18:53 +0300

    Создали проект Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая
+// принимает на вход число и проверяет кратно ли оно оновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число :" );
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// Решение через int
+int Multipule (int num)  
+{
+    int num1 = num % 7;
+    int num2 = num % 23;
+    return (num1+num2);
+}
+
+int result = Multipule(userNum);
+if(result == 0)
+{
+    Console.WriteLine ("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно!");    
+}
+// Решение через int
+
+// // Решение через bool.
+// bool MultipleTwoNumber (int num, int num1, int num2)  
+// {
+//     return num % num1 == 0 && num % num2 == 0;
+// }
+
+// int firstNum = 7;
+// int secomdNum = 23;
+
+// bool result = MultipleTwoNumber(userNum, firstNum,secomdNum);
+// Console.WriteLine(result ? $"{userNum} -> да " : $"{userNum} -> нет ");
+// // Решение через bool.
+
