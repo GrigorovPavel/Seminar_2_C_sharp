ecea714b1d96509568daddef41bc9be4dcfd97c6
Pavel <grigorov_ps@mail.ru>
2023-10-06 17:32:35 +0300

    Создали проект Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿// 13: Напишите программу, которая 
+// 1. принимает на вход любое число и 
+// 1. выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int ThirdNumber (int number)
+{
+    while (number > 999)
+    {
+        number = number / 10; 
+    }
+    return number % 10;
+}
+
+bool VerificationThirdNumber(int number)
+{
+    if(number<100)
+    {
+        Console.WriteLine($"{number} -> третьей цифры нет.");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+Console.WriteLine("Please enter number:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (VerificationThirdNumber(userNumber))
+{
+    Console.Write($"{userNumber} -> " );
+    Console.WriteLine (ThirdNumber(userNumber));
+}
+
+
+
+
